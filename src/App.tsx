9f7be78a9d924fa18a5c2665a4936a946056c231
hrsk <hrsk.yegor@gmail.com>
2023-12-03 18:53:21 +0300

    Box, Link

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,16 @@ import {Rotate} from "./styles/animations";
 
 export function App() {
     return (
-        <div className="App">
+        <Box>
             <h3>STYLED COMPONENTS PROJECT</h3>
             <Button onClick={() => alert('is clicked')}>FIRST STYLED COMPONENT</Button>
-        </div>
+            <SupperButton onClick={() => alert('is clicked')}>SUPER BUTTON</SupperButton>
+            {/*
+            спросить как стилизовать такую ссылку
+            */}
+            <Button as={'a'} href={'#'} onClick={() => alert('is clicked')}>BUTTON LINK</Button>
+            <Button as={Link} href={'#'} onClick={() => alert('is clicked')}>LINK BUTTON</Button>
+        </Box>
     );
 }
 
@@ -25,4 +31,23 @@ const Button = styled.button`
     cursor: pointer;
     animation: ${Rotate} 2s ease-in-out;
   }
+`
+
+const SupperButton = styled(Button)`
+    background-color: burlywood;
+`
+const Link = styled.a`
+  background-color: chartreuse;
+  text-align: center;
+  text-decoration: none;
+`
+
+const Box = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  background-color: cadetblue;
+  gap: 10px;
 `
