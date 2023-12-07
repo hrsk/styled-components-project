e0325851d3af4b072819868899f160c01e5f9e8b
hrsk <hrsk.yegor@gmail.com>
2023-12-07 22:04:38 +0300

    Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
 import styled from 'styled-components';
-import {Box, Button, Link} from "./styles";
+import {Box, Button} from "./styles";
 
 export function App() {
     return (
         <Box>
             <h3>STYLED COMPONENTS PROJECT</h3>
-            <Button onClick={() => alert('is clicked')}>FIRST STYLED COMPONENT</Button>
+{/*            <Button onClick={() => alert('is clicked')}>FIRST STYLED COMPONENT</Button>
             <SupperButton onClick={() => alert('is clicked')}>SUPER BUTTON</SupperButton>
             <Button as={'a'} href={'#'} onClick={() => alert('is clicked')}>BUTTON LINK</Button>
-            <Button as={Link} href={'#'} onClick={() => alert('is clicked')}>LINK BUTTON</Button>
+            <Button as={Link} href={'#'} onClick={() => alert('is clicked')}>LINK BUTTON</Button>*/}
+
+            <Button primary color={'black'} background={'yellow'} onClick={() => alert('is clicked')}>FIRST STYLED COMPONENT</Button>
+            <Button secondary color={'black'} background={'cyan'} onClick={() => alert('is clicked')}>FIRST STYLED COMPONENT</Button>
+
         </Box>
     );
 }
--- src/styles/button/Button.tsx
+++ src/styles/button/Button.tsx
@@ -1,17 +1,33 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import {Rotate} from "../animations";
 
-export const Button = styled.button`
-  color: #BF4F74;
+type ButtonPropsType = {
+    color?: string
+    background?: string
+    fontSize?: string
+    primary?: boolean
+    secondary?: boolean
+
+}
+export const Button = styled.button<ButtonPropsType>`
+  color: ${props => props.color || '#BF4F74'};
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
   border: 2px solid #BF4F74;
   border-radius: 3px;
   display: block;
+  background-color: ${props => props.background || null};
 
   &:hover {
     cursor: pointer;
-    animation: ${Rotate} 2s ease-in-out;
+    //animation: ${Rotate} 2s ease-in-out;
   }
+  
+  ${props => props.primary && css<ButtonPropsType>`
+    &:hover {
+      cursor: pointer;
+      animation: ${Rotate} 2s ease-in-out;
+    }
+  `}
 `;
